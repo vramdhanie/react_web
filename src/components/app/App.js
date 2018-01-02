431324ff8eedd68a6d2f986478bef3078dbddb4c
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-01-02 16:20:46 -0400

    Add react router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import styled from 'styled-components';
-import bg1 from '../../assets/bg1.png';
-import bg2 from '../../assets/bg2.png';
-import Card from '../card/card';
-import CardHeader from '../card/card.header';
+import Home from '../home/home';
+import Articles from '../articles/articles';
+import Navigation from '../navigation/navigation';
+
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 const Wrapper = styled.div`
   overflow-x: visible;
@@ -14,55 +18,28 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 
-const Section = styled.section`
-   position: relative;
-   min-height:50vh;
-   display: flex;
-   align-items: center;
-   justify-content: center;
-`;
-
-const ImageSection = Section.extend`
-   background-image: url(${props => props.background || ''});
-   background-size: cover;
-   color: #FFFFFF;
-   font-size:3em;
-   text-shadow: 0 0 3px black;
-`;
-
-
 const Main = styled.main`
   flex: 1 0 auto;
 `;
 
 class App extends Component {
   render() {
-    return ([
-      <Wrapper key="1">
-        <Header/>
-        <Main>
-          <Section>
-            <Card raised={true}>
-              <CardHeader>
-                First
-              </CardHeader>
-                Card Body
-            </Card>
-          </Section>
-          <ImageSection background = {bg1}>
-            This is a parallax section
-          </ImageSection>
-          <Section>
-            This is static
-          </Section>
-          <ImageSection background={bg2}>
-            Another Parallax section
-          </ImageSection>
-        </Main>
-      </Wrapper>,
-       <Footer key="2">
-        &copy; Vincent Ramdhanie
-       </Footer>]
+    return (
+        <Router>
+          <div>
+          <Wrapper key="1">
+            <Navigation/>
+            <Route exact path="/" component={Header}/>
+            <Main>
+              <Route exact path="/" component={Home}/>
+              <Route path="/articles" component={Articles}/>
+            </Main>
+          </Wrapper>
+           <Footer key="2">
+            &copy; Vincent Ramdhanie
+           </Footer>
+          </div>
+        </Router>
     );
   }
 }
