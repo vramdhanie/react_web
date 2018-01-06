c12e4aa4167eb862a11319d317921dae9af2c481
Vincent Ramdhanie <vramdhanie@gmail.com>
2018-01-06 12:22:11 -0400

    Start styling the navigation bar

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,23 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import colours from '../../shared/constants/colours';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Container = styled.nav`
   background-color: ${colours.PRIMARY_COLOUR_DARK};
   height: 5vh;
 `;
 
-export class Navigation extends Component{
-    render(){
-        return (
-            <Container>
-              The Navigation bar
-              <Link to="/">Home</Link>
-              <Link to="/articles">Articles</Link>
-            </Container>
-        );
-    }
+const NavList = styled.ul`
+  list-style: none;
+  text-align: center;
+  padding: 0;
+  margin: 0;
+  color: ${colours.PRIMARY_COLOUR_TEXT}
+`;
+
+const NavItem = styled.li`
+  line-height: 40px;
+  height: 40px;
+  display: inline-block;
+`;
+
+export class Navigation extends Component {
+  render() {
+    return (
+        <Container>
+          <NavList>
+            <NavItem><Link to="/">Home</Link></NavItem>
+            <NavItem><Link to="/articles">Articles</Link></NavItem>
+          </NavList>
+
+
+        </Container>
+    );
+  }
 }
 
 export default Navigation;
